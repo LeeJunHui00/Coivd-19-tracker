fc3c6bc96a13927a3659ef5b37a6df9ec04c7e3e
LeeJunHui00 <62248164+LeeJunHui00@users.noreply.github.com>
2020-12-03 11:52:10 +0900

    1:35:11

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   MenuItem,
   FormControl,
@@ -9,17 +9,52 @@ import {
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+
+  useEffect(() => {    
+    const getCountriesData = async () => {
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+            name : country.country,
+            value : country.countryInfo.iso2,
+          }));
+
+          setCountries(countries);
+        });
+      };
+      
+      getCountriesData();
+    }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  }
+
   return (
     <div className="app">
-      <h1>Lets build a Covid 19 tracker</h1>
-      <FormControl className="app__dropdown">
-        <Select variant="outlined" value="abc">
-          <MenuItem value="Worldwide">Worldwide</MenuItem>
-          <MenuItem value="Worldwide">option 2</MenuItem>
-          <MenuItem value="Worldwide">option 3</MenuItem>
-          <MenuItem value="Worldwide">option 4</MenuItem>
-        </Select>
-      </FormControl>
+      <div className="app__header">
+        <h1>Lets build a Covid 19 tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="Worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      
+      <div className="app__stats">
+        {/* InforBoxs title="coronavirus cases" */}
+        {/* InforBoxs title="coronavirus recoveries" */ }
+        {/* InfoBoxs */}
+      </div>
+
     </div>
   );
 }
---
+++ src/InfoBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InfoBox({title, case, total}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default InfoBox
