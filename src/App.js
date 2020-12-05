8a4f3c6610f793d79cf6c9f6b80d6ae248b75024
LeeJunHui00 <62248164+LeeJunHui00@users.noreply.github.com>
2020-12-05 23:30:56 +0900

    2:05:52

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import './App.css';
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
 
 
   useEffect(() => {    
@@ -32,10 +33,23 @@ function App() {
       getCountriesData();
     }, []);
 
-  const onCountryChange = (event) => {
+  const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-    setCountry(countryCode);
-  }
+
+    const url = 
+      countryCode === "worldwide" 
+        ? "https://disease.sh/v3/covid-19/all" 
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    
+    await fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      })
+  };
+
+  console.log("Country InFo >>", countryInfo);
 
   return (
     <div className="app">
@@ -57,9 +71,14 @@ function App() {
           <InfoBox title="Recovered" cases={1234} total={3000}/>
           <InfoBox title="Deaths" cases={12345} total={4000}/>
         </div>
-
         <Map /> 
       </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by country</h3>
+          <h3>Worldwide new cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
