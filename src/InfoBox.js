2e33ad44f3a7aa6025194d43be5632abb1709a6e
LeeJunHui00 <62248164+LeeJunHui00@users.noreply.github.com>
2020-12-21 11:16:30 +0900

    4:19:00

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -11,10 +11,10 @@ function InfoBox({title, cases, active, isRed, total, ...props}) {
     return (
         <Card
             onClick={props.onClick}
-            className={`infoBox ${active && "infoBox--selected"}`}
+            className={`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"
+            }`}
         >
             <CardContent>
-
                 <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
 
                 <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
